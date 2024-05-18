85d23f7bc43cda24b08654b2e75e63aade3e7830
JC-comp <alan@jcomp.tk>
2024-05-18 18:57:55 +0800

    feat: Add ads management by channel

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 <a href="https://chrome.google.com/webstore/detail/jadskip-adblock-for-youtu/kcgbbbjpeiaaiokphejajemebphalmii" target="_blank">
     <img alt="Chrome" src="https://img.shields.io/chrome-web-store/stars/kcgbbbjpeiaaiokphejajemebphalmii?label=Chrome&logo=google-chrome&logoColor=white&style=for-the-badge" />
 </a>
+
+YouTube AdBlocker for Chrome.
+
+A 100% free AdBlocker for Youtube and Youtube Music.
+
+JadSkip is a Chrome extensions that saves your time watching ads on Youtube and Youtube Music.
+
+==============================
+
+Features
+
+* Help you skip ads from Youtube
+* Help you skip ads from Youtube Music
+* Help you click the Skip Ads buttons
+* Manage ads by channels
--- src/js/content.js
+++ src/js/content.js
@@ -20,8 +20,44 @@ const clickSkipButtons = (adsModule, name) => {
         buttons[i].click();
 }
 
+const get_channel_id = () => {
+    var channel_id = null;
+    var renderer = document.getElementsByTagName('ytd-video-owner-renderer');
+    if (renderer.length > 0) {
+        renderer = renderer[0];
+        var segs = renderer.querySelector('a').href.split('/');
+        channel_id = segs[segs.length - 1];
+    }
+    if (renderer.length == 0) {
+        renderer = document.querySelector('span[itemprop="author"] link[itemprop=url]')
+        if (renderer) {
+            var segs = renderer.href.split('/');
+            channel_id = segs[segs.length - 1];
+        }
+    }
+    return channel_id;
+}
+
+const check_subscribe = (callback) => {
+    var channel_id = get_channel_id();
+    if (channel_id == null) {
+        callback();
+    } else {
+        chrome.storage.sync.get({subscribes: {}}, function (result) {
+            var subscribes = result.subscribes;
+            if (subscribes[channel_id] === true) {
+                // pass
+            } else {
+                callback();
+            }
+        });
+    }
+}
+
 const check_ads = (cached_url) => {
     const moviePlayer = document.getElementById('movie_player');
+    if (!moviePlayer)
+        return cached_url;
     const videoStream = moviePlayer.getElementsByClassName('video-stream');
     var adsModule = moviePlayer.getElementsByClassName('ytp-ad-module');
     if (videoStream.length && adsModule.length && hasAds(adsModule)) {
@@ -41,9 +77,11 @@ const check_interval = (cached_url, current_interval) => {
     if (current_interval > MAX_MUTATE_INTERVAL)
         return;
     let old_cached_url = cached_url;
-    cached_url = check_ads(cached_url);
-    if (old_cached_url == cached_url)
-        setTimeout(() => check_interval(cached_url, current_interval + 1), MUTATE_TIMEOUT);
+    check_subscribe(() => {
+        cached_url = check_ads(cached_url);
+        if (old_cached_url == cached_url)
+            setTimeout(() => check_interval(cached_url, current_interval + 1), MUTATE_TIMEOUT);
+    });
 }
 
 const video_listener = () => {
@@ -78,7 +116,9 @@ const detector = async (cached_url) => {
         if (isOn) {
             try {
                 video_listener();
-                cached_url = check_ads(cached_url)
+                check_subscribe(() => {
+                    cached_url = check_ads(cached_url)
+                })
             } catch (e) {
                 console.log(e);
             }
@@ -89,3 +129,43 @@ const detector = async (cached_url) => {
 }
 
 detector("");
+
+// render
+function toggle(injected) {
+    var checkbox = injected.querySelector('#status');
+    var channel_id = get_channel_id();
+    checkbox.addEventListener('change', function() {
+        chrome.storage.sync.get({subscribes: {}}, function (result) {
+            var subscribes = result.subscribes;
+            subscribes[channel_id] = !checkbox.checked;
+            chrome.storage.sync.set({subscribes: subscribes}, function() {
+                updateDisplay(injected, channel_id);
+            });
+        });
+    });
+    updateDisplay(injected, channel_id);
+}
+
+function render() {
+    var holder = document.getElementById('subscribe-button');
+    if (!holder)
+        return;
+    var injected = holder.querySelector('.toggle-holder');
+    if (injected)
+        return
+    
+    injected = document.createElement('div');
+    injected.innerHTML = `<div class="toggle-holder">
+        <img id="disabled_img" class="disabled hide" src="chrome-extension://${chrome.runtime.id}/assets/icon-120.png">
+        <div class="toggle-switch" style="margin: 0 3px;">
+            <label class="switch">
+                <input id="status" type="checkbox">
+                <span class="slider"></span>
+            </label>
+        </div>
+        <img id="enabled_img"class="hide" src="chrome-extension://${chrome.runtime.id}/assets/icon-120.png">
+    </div>`;
+    toggle(injected);
+    holder.insertBefore(injected, holder.firstChild);
+}
+setInterval(render, 1000);
